6e22a4a66201e0260fa28c671b64c5911bdc770c
Christopher Friesen <12847512+parlance-zz@users.noreply.github.com>
2022-09-11 04:11:22 -0600

    Updated !help command finally

--- g_diffuser_bot_params.py
+++ g_diffuser_bot_params.py
@@ -4,14 +4,15 @@
 # Discord developers site: https://discordapp.com/developers/applications/ instructions: https://www.writebots.com/discord-bot-token/
 BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
 BOT_ADMIN_ROLE_NAME = "ADMIN_ROLE_NAME_HERE"  # IMPORTANT - use your server / guild group names here accordingly, check the help text for command permissions
-BOT_USERS_ROLE_NAME = "USER_ROLE_NAME_HERE"   # if you want anyone to be able to use the bot, change this to "everyone"
+BOT_USERS_ROLE_NAME = "USER_ROLE_NAME_HERE" # if you want anyone to be able to use the bot, change this to "everyone"
 BOT_COMMAND_PREFIX = "!"
 BOT_ACTIVITY = "The way of the future..."
 BOT_USE_OPTIMIZED = False                     # IMPORTANT - Set to true if you encounter errors running commands due to low memory
 
 # IMPORTANT - Sign up for an account on https://www.huggingface.co and accept the terms to use stable diffusion
 # Create an access token at https://huggingface.co/settings/tokens and enter your token below:
-HUGGINGFACE_TOKEN = "YOUR_HUGGINGFACE_ACCESS_TOKEN"     # if you use a locally downloaded diffusers model you can comment this line out
+HUGGINGFACE_TOKEN = "YOUR_HUGGINGFACE_ACCESS_TOKEN"   # if you use a locally downloaded diffusers model you can comment this line out
+#HUGGINGFACE_TOKEN = None
 CMD_SERVER_MODEL_NAME = "CompVis/stable-diffusion-v1-4"
 #CMD_SERVER_MODEL_NAME = "./stable-diffusion-v1-4"      #  if you use a locally downloaded diffusers model then make this the relative path to the downloaded folder"
                        
@@ -25,7 +26,7 @@ BOT_LOG_FILE_PATH = ROOT_PATH + "/g-diffuser-bot.log"            # keep a text l
 SERVER_LOG_FILE_PATH = ROOT_PATH + "/diffuser-server.log"
 
 # default params for commands, these are overriden by any user supplied params
-DEFAULT_CMD_PARAMS = { "-n": 1, "-steps": "50", "-scale": "10.", "-str": "0.5" }
+DEFAULT_CMD_PARAMS = { "-n": 1, "-steps": "50", "-scale": "10.", "-str": "0.8", "-color": "0." }
 AUTO_SEED_RANGE = (1,999999)
 MAX_STEPS_LIMIT = 300
 MAX_REPEAT_LIMIT = 100             # max number of repetitions that can be used with the -x param
@@ -46,7 +47,7 @@ TMP_CLEAN_PATHS = [
 ]
 
 # merged list of all valid options / parameters for all commands
-PARAM_LIST = ["-str", "-scale", "-seed", "-steps", "-x", "-mine", "-all", "-num", "-force", "-user", "-w", "-h", "-n", "-none", "-color", "-noise_q"]
+PARAM_LIST = ["-str", "-scale", "-seed", "-steps", "-x", "-mine", "-all", "-num", "-force", "-user", "-w", "-h", "-n", "-none", "-color", "-noise_q", "-blend"]
 
 # by default the command server runs and binds to localhost for local connections only
 CMD_SERVER_BIND_HTTP_HOST = "localhost"
@@ -71,49 +72,51 @@ For more information on the G-Diffuser-Bot please see https://github.com/parlanc
 
 HELP_TXT1 = """
 User Commands:
-  !t2i : Generates an image with a prompt [-seed num] [-scale num] [-steps num][-x num]
-  !t2ihd : As above, uses txt2imghd to generate 1 sample at 4x size
-  !i2i : Generates an image with a prompt and input image [-seed num] [-str num] [-scale num] [-steps num] [-x num] 
-  !enhance : Uses esrgan to upscale the input image image by 4x
+  !gen : Generates a new sample with the given prompt, parameters, and input attachments
   !queue : Shows running / waiting commands in the queue [-mine]
-  !cancel : Cancels your last command (can be used while running) [-all]
+  !cancel : Cancels your last command, or optionally a specific number of commands (can be used while running) [-all]
   !top : Shows the top users' total running time
-  !select : Selects an image by number from your last result and make it your input image (left to right, top to bottom) (skips the queue)
+  !select : Crops an image by number from your last result and make it your input image (left to right, top to bottom) [-none]
   !show_input : Shows your current input image (skips the queue)
  
 Admin Commands:
   !shutdown : Cancel all pending / running commands and shutdown the bot (can only be used by bot owner)
-  !clean : Delete temporary files in SD folders, -force will delete temporary files that may still be referenced (can only be used by bot owner) [-force]
+  !clean : Delete temporary files in SD folders, [-force] will delete temporary files that may still be referenced (can only be used by bot owner) [-force]
   !restart : Restart the bot after the command queue is idle
-  !clear [user]: Cancel all or only a specific user's pending / running commands
+  !clear : Cancel all or only a specific user's pending / running commands [-all] [-user]
+  
 """
 HELP_TXT2=""" 
 Parameter Notes:
   -seed : Any whole number (default random)
-  -scale : Can be any positive real number (default 6). Controls the unconditional guidance scale. Good values are between 3-20.
-  -str : Number between 0 and 1, (default 0.4). Controls how much to change the input image. 
-  -plms : Use the plms instead of ddim sampler to generate your output.
-  -steps: Any whole number from 10 to 200 (default 50). Controls how many times to recursively change the input image.
-  -x: Repeat the given command some number of times. Instead of a number, use -x samplers to repeat the command with every sampler.
-
-Models and Samplers:
- - !t2i supports alternate samplers, and all generation commands support alternate models with [-m model] (sd1.4_small)
- - To use an alternate sampler use the following options [-plms] [-dpm_2_a] [-dpm_2] [-euler_a] [-euler] [-heun] [-lms]
- 
-Input images:
-  Commands that require an input image will use the image you attach to your message. If you do not attach an image it will attempt to use the last image you attached.
-  Input images will be cropped to 1:1 aspect and resized to 512x512.
-  The select command can be used to turn your last command's output image into your next input image, please see !select above.
-
+  -scale : Can be any positive real number (default 10). Controls the unconditional guidance scale. Good values are between 3-20.
+  -str : Number between 0 and 1, (default 0.5). Controls how much to change the input image. 
+  -steps : Any whole number from 10 to 300 (default 50). Controls how many times to iteratively refine the sample.
+  -x : Repeat the given command some number of times.
+  -w : Set the output width  (this will be rounded to a multiple of 64)
+  -h : Set the output height (this will be rounded to a multiple of 64)
+  -n : Choose the number of samples to generate at once
+  -color : How much color variation to add when in/out-painting, if you use this try small values (0..1, default 0.)
+  -noise_q : Controls the exponent in the in/out-painting noise distribution. Higher values means larger features and lower values means
+             smaller features. (range > 0., default 1.)
+  -blend : Can be used to adjust mask hardness when in/out-painting, higher values is sharper (range >= 1., default 1.)
+  
 Examples:
   To see examples of valid commands use !examples
 """
 
 EXAMPLES_TXT = """
 Example commands:
-!t2i an astronaut riding a horse on the moon
-!t2i painting of an island by lisa frank -plms -seed 10
-!t2ihd baroque painting of a mystical island treehouse on the ocean, chrono trigger, trending on artstation, soft lighting, vivid colors, extremely detailed, very intricate -plms
-!t2i my little pony in space marine armor from warhammer 40k, trending on artstation, intricate detail, 3d render, gritty, dark colors, cinematic lighting, cosmic background with colorful constellations -scale 10 -seed 174468 -steps 50
-!t2ihd baroque painting of a mystical island treehouse on the ocean, chrono trigger, trending on artstation, soft lighting, vivid colors, extremely detailed, very intricate -scale 14 -str 0.375 -seed 252229
+!gen an astronaut riding a horse on the moon
+!gen painting of an island by lisa frank -seed 10
+!gen baroque painting of a mystical island treehouse on the ocean, chrono trigger, trending on artstation, soft lighting, vivid colors, extremely detailed, very intricate
+!gen my little pony in space marine armor from warhammer 40k, trending on artstation, intricate detail, 3d render, gritty, dark colors, cinematic lighting, cosmic background with colorful constellations -scale 10 -seed 174468 -steps 50
+!gen baroque painting of a mystical island treehouse on the ocean, chrono trigger, trending on artstation, soft lighting, vivid colors, extremely detailed, very intricate -scale 14 -seed 252229
+"""
+
+
+"""
+Input images:
+  Commands that require an input image will use the image you attach to your message. If you do not attach an image it will attempt to use the last image you attached.
+  The select command can be used to turn your last command's output image into your next input image, please see !select above.
 """
