908c69c27909ba08590d35a8265bf2a0f043b81c
Christopher Friesen <parlance@fifth-harmonic.com>
2022-09-21 20:40:26 -0600

    added optional arg to load_args to load another json

--- g_diffuser_cli.py
+++ g_diffuser_cli.py
@@ -202,10 +202,11 @@ def cli_show_args():
     print("Current args: " + str(gdl.strip_args(INTERACTIVE_CLI_ARGS))+"\n")
     return
     
-def cli_load_args():
+def cli_load_args(name=""):
     global INTERACTIVE_CLI_ARGS
     try:
-        saved_args_dict = gdl.load_debug_json("last_sample_args")
+        if not name: name = "last_sample_args"
+        saved_args_dict = gdl.load_debug_json(name)
         INTERACTIVE_CLI_ARGS = argparse.Namespace(**gdl.merge_dicts(vars(INTERACTIVE_CLI_ARGS), saved_args_dict))
         print("Loaded args from file: " + str(gdl.strip_args(INTERACTIVE_CLI_ARGS))+"\n")
     except Exception as e:
