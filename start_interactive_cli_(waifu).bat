0323fcbb56175fd609ddba5e55a0772473234e3d
Christopher Friesen <parlance@fifth-harmonic.com>
2022-09-29 03:37:35 -0600

    removed bat file no longer needed

--- start_interactive_cli_(waifu).bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd %0\..\
-REM todo: maybe check if the waifu-diffusion model is downloaded and display error if it isn't
-cmd /k "conda activate g_diffuser & python g_diffuser_cli.py --interactive --model-name waifu-diffusion"
