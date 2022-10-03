c07c1eec7fc5b4d34b691aee096aeb5f61c44410
Christopher Friesen <parlance@fifth-harmonic.com>
2022-10-03 11:11:55 -0600

    updated default cfg

--- g_diffuser_defaults.py
+++ g_diffuser_defaults.py
@@ -5,15 +5,15 @@ import argparse
 DEFAULT_SAMPLE_SETTINGS = argparse.Namespace()
 DEFAULT_SAMPLE_SETTINGS.sampler = "k_euler"                  # default sampling mode (ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_lms)
 DEFAULT_SAMPLE_SETTINGS.n = 1                                # number of samples to generate per sample command by default
-DEFAULT_SAMPLE_SETTINGS.batch_size = 8                       # this batch size is only used when running a repeating command (n <= 0)
+#DEFAULT_SAMPLE_SETTINGS.batch_size = 8                      # this batch size is only used when running a repeating command (n <= 0)
 DEFAULT_SAMPLE_SETTINGS.resolution = (512,512)               # default resolution for img / video outputs
-DEFAULT_SAMPLE_SETTINGS.max_resolution = (1024,1024)           # if you run out of ram due to output resolution lower this to prevent exceeding your max memory
+DEFAULT_SAMPLE_SETTINGS.max_resolution = (1024,1024)         # if you run out of ram due to output resolution lower this to prevent exceeding your max memory
 DEFAULT_SAMPLE_SETTINGS.resolution_granularity = 64          # required by diffusers stable-diffusion for now due to latent space subsampling
 DEFAULT_SAMPLE_SETTINGS.noise_start = 0.42                   # default strength for pure img2img or style transfer
 DEFAULT_SAMPLE_SETTINGS.noise_end = 0.01                     # can be used to influence in/out-painting quality
 DEFAULT_SAMPLE_SETTINGS.noise_eta = 0.70                     # can be used to influence in/out-painting quality
-DEFAULT_SAMPLE_SETTINGS.scale = 11.                          # default cfg scale
-DEFAULT_SAMPLE_SETTINGS.steps = 32                           # default number of sampling steps, lower to reduce sampling time
+DEFAULT_SAMPLE_SETTINGS.scale = 8.                           # default cfg scale
+DEFAULT_SAMPLE_SETTINGS.steps = 16                           # default number of sampling steps, lower to reduce sampling time
 DEFAULT_SAMPLE_SETTINGS.noise_q = 1.                         # fall-off of shaped noise distribution for in/out-painting
 DEFAULT_SAMPLE_SETTINGS.auto_seed_range = (10000,99999)      # automatic random seed range
 DEFAULT_SAMPLE_SETTINGS.model_name = "stable-diffusion-v1-4" # default model id to use, see g_diffuser_config_models.yaml for the list of models to be loaded by grpc server
