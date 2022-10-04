e4880cbb5a7260db708a2606cd3588bc10097371
Christopher Friesen <parlance@fifth-harmonic.com>
2022-10-04 03:19:51 -0600

    added script to start grpc server headless

---
+++ g_diffuser_start_server.py
@@ -0,0 +1,40 @@
+"""
+MIT License
+
+Copyright (c) 2022 Christopher Friesen
+https://github.com/parlance-zz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+g_diffuser_start_server.py - start the GRPC server headless. useful for debugging or starting and stopping multiple clients
+                             without having to reload all the grpc server resources
+
+"""
+
+import ntpath; ntpath.realpath = ntpath.abspath # can help with long paths in certain python environments
+
+from g_diffuser_config import DEFAULT_PATHS
+import os; os.chdir(DEFAULT_PATHS.root)
+import time
+
+import g_diffuser_lib as gdl
+gdl.start_grpc_server(gdl.get_default_args())
+while True:
+    time.sleep(10)
