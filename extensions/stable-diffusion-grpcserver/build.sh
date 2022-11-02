c530167ddf3eff7560a2ddcb95ca904a80a570b4
Christopher Friesen <parlance@fifth-harmonic.com>
2022-11-02 11:43:06 -0600

    removed (unused) non-client sdgrpcserver files

--- extensions/stable-diffusion-grpcserver/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git submodule update --init --recursive
-python -m grpc_tools.protoc -Isdgrpcserver/proto -Iapi-interfaces/src/proto --python_out=sdgrpcserver/generated --grpc_python_out=sdgrpcserver/generated api-interfaces/src/proto/engines.proto
-python -m grpc_tools.protoc -Isdgrpcserver/proto -Iapi-interfaces/src/proto --python_out=sdgrpcserver/generated --grpc_python_out=sdgrpcserver/generated api-interfaces/src/proto/dashboard.proto
-python -m grpc_tools.protoc -Isdgrpcserver/proto -Iapi-interfaces/src/proto --python_out=sdgrpcserver/generated --grpc_python_out=sdgrpcserver/generated api-interfaces/src/proto/completion.proto
-
-# Our generation.proto is adjusted.
-python -m grpc_tools.protoc -Isdgrpcserver/proto -Iapi-interfaces/src/proto --python_out=sdgrpcserver/generated --grpc_python_out=sdgrpcserver/generated sdgrpcserver/proto/generation.proto
