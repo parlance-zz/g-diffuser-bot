4d37c18cc10196b70ebc33ebc66128880641b369
Christopher Friesen <parlance@fifth-harmonic.com>
2022-12-01 10:47:59 -0700

    add resume param

--- inputs/scripts/zoom_maker.py
+++ inputs/scripts/zoom_maker.py
@@ -1,12 +1,14 @@
 # zoom and  E N H A N C E
 
+import glob
 import shutil
 import numpy as np
 from argparse import Namespace
 
-_cli_args = args
+recycle_image = "zoom_maker.png" # file path in /inputs/ to copy over from outputs for recursive zoom
 
 args = cli_default_args()
+args.zoom_resume = False   # set to True to start generating the zoom from the last keyframe instead of endzoom.png
 args.zoom_prompt_schedule = [
     #"Face portrait of a retrofuturistic assassin surrounded by advanced brutalist architecture. highly detailed fantasy, trending on artstation an ultrafine hyperdetailed colorfull illustration by kim jung gi, moebius, irakli nadar, alphonse mucha, ayami kojima, amano, greg hildebrandt",
     #"a wooden shed, many green plant and flower gowing on it, illustration key visual trending pixiv fanbox by wlop and greg rutkowski and makoto shinkai and studio ghibli",
@@ -30,25 +32,39 @@ args.expand_bottom = 40     # these values are in % of the original image size
 args.expand_left = 40       # exceeding 50% in any direction is not recommended for recursive zooms / pans
 args.expand_right = 40
 
-args.init_image = "endzoom.png"  # starting (or rather, ending image file, relative to inputs path)
+args.init_image = ""  # starting (or rather, ending image file, relative to inputs path). if blank start with a generated image
 args.output_path = "zoom_maker"  # output path, relative to outputs
 args.output_name = "zoom_maker"
-args.steps = 12 #60
+args.steps = 30 #60
 args.cfg_scale = 14.
 args.guidance_strength = 0.5   # try lowering clip guidance_strength if you have problems with zooms "exploding"
 #args.negative_prompt = "frame, comic book, collage, cropped, oversaturated, signed, greyscale, monotone, vignette, title, text, logo, watermark"
 args.negative_prompt = "watermark, title, label, logo, collage, cropped, oversaturated, monotone, vignette"
-#args.sampler = "k_euler_ancestral"
+args.sampler = "k_euler_ancestral"
 #args.sampler = "k_dpm_2_ancestral"
 #args.sampler = "dpmspp_2"
-args.sampler = "dpmspp_3"
+#args.sampler = "dpmspp_3"
 
-if _cli_args: # if an args object was passed in the cli run() command merge those parameters into the defaults
-    args = Namespace(**(vars(args) | vars(_cli_args)))
+# if args or keyword args were passed in the cli run command, override the defaults
+if cli_args: args = Namespace(**(vars(args) | vars(cli_args)))
+if kwargs: args = Namespace(**(vars(args) | kwargs))
+
+if args.zoom_resume:
+    frame_filenames = sorted(glob.glob(gdl.DEFAULT_PATHS.outputs+"/"+args.output_path+"/*.png"), reverse=True)
+    if len(frame_filenames) > 0:
+        args.init_image = recycle_image
+        output_file = frame_filenames[0]
+        input_file = gdl.DEFAULT_PATHS.inputs+"/"+args.init_image
+        print("Resuming zoom from {0}...".format(output_file))
+        print("Copying from {0} to {1}...".format(output_file, input_file))
+        shutil.copyfile(output_file, input_file)
+    else:
+        raise Exception("Error: No frames found in {0} to resume from!".format(gdl.DEFAULT_PATHS.outputs+"/"+args.output_path))
+else:
+    if not args.init_image: print("No init_image specified, generating a random starting image...")
+    else: print("Starting from '{0}'...".format(args.init_image))
 
 # *****************************************************************
-
-recycle_image = "zoom_maker.png" # this file/path is used to copy the last output image to the inputs folder
 
 # create zoom frames
 for i in range(args.zoom_num_frames):
@@ -66,8 +82,7 @@ for i in range(args.zoom_num_frames):
         print("prompt: {0}".format(args.prompt))
 
     args = sample(args)
-    if not args.output_file:
-        break # cancelled or error
+    if args.status != 2: break # cancelled or error
 
     # recycle the output back into the next input init_image for a recursive zoom effect
     args.init_image = recycle_image
